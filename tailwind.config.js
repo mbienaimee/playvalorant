b2f35b0cc20c801b4d245314fd3fbb746de014eb
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-02-13 19:50:57 +0200

    projext

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: ["#111"],
+      },
+    },
+  },
+  plugins: [],
+};
